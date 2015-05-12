c82c8fa7fe9446492e85bb86146359a2c1b93446
Johannes Stein <johannesstein@freeze-dev.com>
2015-05-12 11:34:42 +0200

    Now with text inputs

--- Card.js
+++ Card.js
@@ -4,8 +4,9 @@ var React = require('react-native');
 
 var {
   StyleSheet,
-  Text,
+  TextInput,
   View,
+  PanResponder,
 } = React;
 
 var Card = React.createClass({
@@ -19,10 +20,41 @@ var Card = React.createClass({
       title: this.props.initialTitle
     };
   },
+  componentWillMount: function() {
+  	this._panGesture = PanResponder.create({
+      // Ask to be the responder:
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+
+      onPanResponderGrant: (evt, gestureState) => {
+        // The guesture has started. Show visual feedback so the user knows
+        // what is happening!
+
+        // gestureState.{x,y}0 will be set to zero now
+      },
+      onPanResponderMove: (evt, gestureState) => {
+        // The most recent move distance is gestureState.move{X,Y}
+
+        // The accumulated gesture distance since becoming responder is
+        // gestureState.d{x,y}
+      },
+      onPanResponderTerminationRequest: (evt, gestureState) => true,
+      onPanResponderRelease: (evt, gestureState) => {
+        // The user has released all touches while this view is the
+        // responder. This typically means a gesture has succeeded
+      },
+      onPanResponderTerminate: (evt, gestureState) => {
+        // Another component has become the responder, so this gesture
+        // should be cancelled
+      },
+    });
+  },
   render: function() {
     return (
       <View style={styles.card}>
-        <Text style={styles.cardText}>{this.state.title}</Text>
+        <TextInput style={styles.cardText} value={this.state.title} onChangeText={(text) => this.setState({title: text})} />
       </View>
       );
   }
@@ -41,6 +73,7 @@ var styles = StyleSheet.create({
     alignItems: 'center'
   },
   cardText: {
+  	flex: 1,
     fontSize: 20
   }
 });
